6eb8357ccb1d10345a52ea19e4b308e6452e727c
Anton <antonsapovalov0607@gmail.com>
2025-01-29 20:35:04 +0200

    test_5

--- js/logic.js
+++ js/logic.js
@@ -6,3 +6,16 @@ document.getElementById('menu_toggle').addEventListener('change', function () {
         menuBox.classList.remove('active');
     }
 });
+
+document.querySelector(".footer_form").addEventListener("submit", function (event) {
+    event.preventDefault(); 
+
+    const nameField = document.querySelector('.footer_inputs[type="text"]').value.trim();
+    const emailField = document.querySelector('.footer_inputs[type="email"]').value.trim();
+
+    if (nameField && emailField) {
+        alert("Дякую! Ви успішно підписалися!");
+    } else {
+        alert("Будь ласка, заповніть всі поля перед відправкою.");
+    }
+});
